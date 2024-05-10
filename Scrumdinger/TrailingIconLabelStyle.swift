d1133a079cdd9ed34c4a696dff98ec0f52e1da99
mrueda020 <mrueda020@hotmail.com>
2024-05-10 17:49:45 -0600

    Add CardView

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Miguel on 10/05/24.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
