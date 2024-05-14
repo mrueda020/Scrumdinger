f64b0c5b5de1bcaf81b52dc6c54e2f33f38ba22f
Miguel Rueda Carbajal <46406586+mrueda020@users.noreply.github.com>
2024-05-13 20:13:21 -0600

    Use bindings

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
+    
+    @State private var editingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
+    
     var body: some View {
         List{
             Section(header: Text("Meeting Info")) {
@@ -48,11 +51,12 @@ struct DetailView: View {
         .toolbar {
             Button("Edit") {
                 isPresentingEditView = true
+                editingScrum = scrum
             }
         }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationStack {
-                DetailEditView()
+                DetailEditView(scrum: $editingScrum)
                     .navigationTitle(scrum.title)
                     .toolbar {
                         ToolbarItem(placement: .cancellationAction) {
@@ -63,6 +67,7 @@ struct DetailView: View {
                         ToolbarItem(placement: .confirmationAction) {
                             Button("Done") {
                                 isPresentingEditView = false
+                                scrum = editingScrum
                             }
                         }
                     }
@@ -73,6 +78,6 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(scrum: DailyScrum.sampleData[0])
+        DetailView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
